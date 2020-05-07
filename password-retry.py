1a59eaddacd078c067bb33ba890b9ade222f3249
Dawei Huang <dv888000@gmail.com.tw>
2020-05-07 11:14:26 -0400

    basic version

---
+++ password-retry.py
@@ -0,0 +1,16 @@
+password = 'a123456'
+i = 3
+while True :
+	pwd = input ('請輸入密碼:')
+	if pwd == password:
+		print ('登入成功')
+		break
+	else:
+		i = i - 1
+		print ('您還剩餘', i,'次機會')
+		if i == 0 :
+			print ('密碼嘗試錯誤已達上限,帳戶將被鎖定')
+			break
+
+
+
